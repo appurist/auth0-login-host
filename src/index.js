0e321d4c17d5165377a79abe186f1b46a218b019
Paul Whittemore <dev@appurist.com>
2022-05-03 16:24:22 -0300

    Test for and use /go as built-in ?deskReturnTo= handler.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const redirectUri = `${window.location.origin}${window.location.pathname}`;
-
 console.log("ClientID:", clientId);
+
+// This is the path that handles redirection via ?deskReturnTo= after Auth0 returns.
+const REDIRECT_PATH = "/go";
+// const redirectUri = `${window.location.origin}${window.location.pathname}`;
+const redirectUri = window.location.origin + REDIRECT_PATH;
+
+if (window.location.pathname === REDIRECT_PATH) {
+  let returnTo = new URLSearchParams(window.location.search).get("deskReturnTo");
+  if (returnTo) {
+    console.log("/go is recognizing deskReturnTo:", returnTo);
+    if (!returnTo.startsWith("http")) {
+      returnTo = window.location.origin + returnTo;
+    }
+    window.location.assign(returnTo);
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -16,7 +30,10 @@ const onRedirectCallback = async (appState) => {
   if (appState?.target) {
     // appState redirection support through Auth0    
     // const newTarget = "https://account-dev.coindesk.com/login?deskReturnTo=" + appState.target;
-    const newTarget = window.location.origin + "/login?deskReturnTo=" + appState.target;
+    
+    // i.e. coindesk.com/go?deskReturnTo=desiredPath
+    const newTarget = window.location.origin + REDIRECT_PATH + "?deskReturnTo=" + appState.target;
+
     console.log(`onRedirectCallback: target="${newTarget}", appstate =`, JSON.stringify(appState, null, 2));
     window.history.replaceState({}, '',  newTarget);
   }
